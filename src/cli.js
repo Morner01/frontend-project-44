47775c7df61a0d849aa0dc530f5946172321aad9
Morner01 <van.maltsev2005@yandex.ru>
2025-11-30 21:53:29 +0300

    v20

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const helloUser = () => {
-  // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  // eslint-disable-next-line no-console
   console.log(`Hello, ${name}!`);
-  // eslint-disable-next-line no-console
   return name;
 };
 export default helloUser;
