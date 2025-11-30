9bc124a4ff00515a932d20cea8f6be84a2578201
Morner01 <van.maltsev2005@yandex.ru>
2025-11-30 20:18:38 +0300

    v12

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,7 +1,7 @@
 import HelloUser from '../cli.js';
 import startGame from '../index.js';
 
-const generateProgression = (minLength = 3, maxLength = 10) => {
+const generateProgression = (minLength = 5, maxLength = 10) => {
   const progressionLength = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
   const start = Math.floor(Math.random() * 99);
   const difference = Math.floor(Math.random() * 99);
